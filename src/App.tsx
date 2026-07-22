32a3d6993c9aa30c4ca657dbdfe777c60ff948a4
dbstjqls <130419093+dbstjqls@users.noreply.github.com>
2026-07-23 03:52:42 +0900

    feat: 이용약관 실제 라우트 등록 (#61)

--- src/App.tsx
+++ src/App.tsx
@@ -27,6 +27,7 @@ import MyReservationListPage from './pages/mypage/MyReservationListPage';
 import MyReservationDetailPage from './pages/mypage/MyReservationDetailPage';
 import NailShopDetailPage from './pages/NailShopDetailPage';
 import MyPage from './pages/mypage/MyPage';
+import TermsDetailView from './components/mypage/TermsDetailView';
 
 const NAV_VISIBLE_PATHS = ['/home', '/art-search', '/wishlist', '/mypage'];
 
@@ -94,6 +95,7 @@ function App() {
           <Route path="/reservations/:reservationId" element={<MyReservationDetailPageRoute />} />
           <Route path="/shop/:shopId" element={<NailShopDetailPageRoute />} />
           <Route path="/SplashPage" element={<SplashPage />} />
+          <Route path="/mypage/terms" element={<TermsDetailView />} />
         </Routes>
       </LayoutWrapper>
     </BrowserRouter>
