aaf032af0d32133837084c2544c8a2063178db87
이혜인 <hunbee776@gmail.com>
2026-07-31 04:37:59 +0900

    refactor. 디자인태그 조회 수정

--- src/data/designTag.ts
+++ src/data/designTag.ts
@@ -16,8 +16,7 @@ interface ApiResponse<T> {
 
 export async function fetchDesignTags(): Promise<DesignTag[]> {
   try {
-    const res = await fetch(`${import.meta.env.VITE_API_BASE_URL}/users/design-moods`);
-//인증필요없는걸로 교체 필요
+      const res = await fetch(`${import.meta.env.VITE_API_BASE_URL}/shops/design-tags`);
     if (!res.ok) {
       console.error(`디자인 태그  실패: ${res.status}`);
       return [];
