343f835a3179be17421663ab3acc3577e41d5eb2
hunbee776 <hunbee776@gmail.com>
2026-08-03 03:47:13 +0900

    refactor. 아트 상세 페이지

--- src/components/mypage/ProfileEdit.tsx
+++ src/components/mypage/ProfileEdit.tsx
@@ -310,7 +310,7 @@ export default function ProfileForm({
             </div>
           ) : (
             <div className="flex items-end gap-2.5">
-              <p className="flex-1 pt-1.5 text-[15px] -translate-y-[4px] text-[#1E2427]">{nicknameValue}</p>
+              <p className="flex-1 pt-1.5 text-[15px] -translate-y-[3px] text-[#1E2427]">{nicknameValue}</p>
               <ChangeButton label="변경하기" onClick={startNicknameEdit} />
             </div>
           )}
